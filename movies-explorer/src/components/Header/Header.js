d0b3e0bc6c703059396ba008d86ddb3aac90b5b6
Milenium666 <whiteangel64rus@gmail.com>
2021-12-13 21:44:23 +0300

    Сверстать header

--- movies-explorer/src/components/Header/Header.js
+++ movies-explorer/src/components/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import './Header.css';
+import logo from '../../images/logo.svg';
+import { Link } from 'react-router-dom';
+
 
 function Header() {
     return(
-        <div>
-            Header — компонент, который отрисовывает шапку сайта на страницу.
+        <header className='header'>
+            <Link to='/' className='header__logo-link'>
+                <img className='header__logo' alt='Логотип сайта' src={logo}/>
+            </Link>
+            <div className='header__links'>
+                <Link to='/signup' className='header__link-signup'>
+                    Регистрация
+                </Link>
+                
+                    <Link to='signin' className='header__link-signin'>
+                        Войти
+                    </Link>
+                
             </div>
+        </header>
     )
 }
 
