fa6d2df4ca799c3d8392c5484625beb70e2cf182
Milenium666 <whiteangel64rus@gmail.com>
2021-12-16 16:43:50 +0300

    сверстан блок AboutMe

--- movies-explorer/src/components/AboutMe/AboutMe.js
+++ movies-explorer/src/components/AboutMe/AboutMe.js
@@ -1,11 +1,45 @@
 import React from 'react';
 import './AboutMe.css';
+import Portfolio from '../Portfolio/Portfolio';
+import photo from '../../images/photo.png';
 
 function AboutMe() {
     return(
-        <div>
-            AboutMe — компонент с информацией о студенте.
+        <section className='about-me'>
+            <h5 className='about-me__title'>
+                Студент
+            </h5>
+            <div className='about-me__line'></div>
+            <div className='about-me__discription'>
+                <div className='student'>
+                    <h6 className='student__name'>
+                        Виталий
+                    </h6>
+                    <h6 className='student__profession'>
+                        Фронтенд-разработчик, 30 лет
+                    </h6>
+                    <p className='student__descrition'>
+                    Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
+    и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.
+                    </p>
+                    <ul className='link'>
+                        <li >
+                            <a className='link__item' href='https://www.facebook.com' target="_blank">
+                                Facebook
+                            </a>
+                        </li>
+                        <li>
+                            <a className='link__item' href='https://github.com' target="_blank">
+                                Github
+                            </a>
+                        </li>
+                    </ul>
+                    
+                </div>
+                <img className='about-me__photo' alt='фото студента' src={photo}/>
             </div>
+            <Portfolio />
+        </section>
     )
 }
 
--- movies-explorer/src/components/Main/Main.js
+++ movies-explorer/src/components/Main/Main.js
@@ -6,7 +6,7 @@ import Promo from '../Promo/Promo'
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
-import Portfolio from '../Portfolio/Portfolio';
+
 
 function Main() {
   return (
@@ -16,7 +16,7 @@ function Main() {
       <AboutProject />
       <Techs />
       <AboutMe />
-      <Portfolio />
+      
     </main>
   );
 }
