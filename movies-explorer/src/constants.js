3478b3ea9b37ff823821d444c8b8d01fe5fd9c74
Milenium666 <whiteangel64rus@gmail.com>
2022-09-05 18:23:12 +0300

    update package-lock.json

--- movies-explorer/src/constants.js
+++ movies-explorer/src/constants.js
@@ -1,7 +1,8 @@
 export const PATTERN_NAME = "[a-zA-Zа-яА-ЯёЁ -]{2,30}";
 export const PATTERN_EMAIL= '^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\\.)+[A-Za-z]+$';
 
-export const MAIN_API = 'https://movies-explorer.elena.nomoredomains.rocks/api';
+// export const MAIN_API = 'https://movies-explorer.elena.nomoredomains.rocks/api';
+export const MAIN_API = 'http://localhost:3000/api'
 
 export const MOVIE_API = 'https://api.nomoreparties.co'
 
